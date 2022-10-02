54c71324b938b3dbac6e856f7e11e1a28df7c859
kimjy97 <99719878+kimjy97@users.noreply.github.com>
2022-10-03 02:29:37 +0900

    Add files via upload

---
+++ getText.php
@@ -0,0 +1,3 @@
+<?php
+echo file_get_contents('http://poot97.dothome.co.kr/myPF/file/' . $_POST["name"]);
+?>
---
+++ request.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_POST["name"]))
+{
+    $data = array('action' => $_POST["action"], 'name' => $_POST["name"], 'contents' => $_POST["contents"], 'byte' => $_POST["byte"]);
+}else{
+    $data = array('action' => $_POST["action"]);
+}
+
+$url = 'http://poot97.dothome.co.kr/myPF/action.php';
+
+$options = array(
+	'http' => array(
+		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		'method'  => 'POST',
+		'content' => http_build_query($data, '' , '&'),
+		'timeout' => 50000
+	)
+);
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+echo $result;
+
+
+//post방식으로 매개변수 전달하여 php파일 불러오는 방법 사용됨.
+?>
