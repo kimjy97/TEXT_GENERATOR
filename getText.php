8b9ac43e23d1d13d7e9fd9b9694ae0a052feb4ee
kimjy97 <99719878+kimjy97@users.noreply.github.com>
2022-10-04 17:58:36 +0900

    Add files via upload

--- getText.php
+++ getText.php
@@ -1,3 +1,15 @@
 <?php
+/* file_get_contents 함수 사용 방법
 echo file_get_contents('http://poot97.dothome.co.kr/TextGenerator/file/' . $_POST["name"]);
+*/
+
+/* 보안 이슈 보완 - cURL 사용 */
+$url = 'http://poot97.dothome.co.kr/TextGenerator/file/' . $_POST["name"];
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
 ?>
--- request.php
+++ request.php
@@ -1,5 +1,6 @@
 <?php
 
+
 if(isset($_POST["name"]))
 {
     $data = array('action' => $_POST["action"], 'name' => $_POST["name"], 'contents' => $_POST["contents"], 'byte' => $_POST["byte"]);
@@ -9,19 +10,34 @@ if(isset($_POST["name"]))
 
 $url = 'http://poot97.dothome.co.kr/TextGenerator/action.php';
 
+/* file_get_contents 방법 사용 ( 문제 : 보안상  오류가 발생함 )
 $options = array(
 	'http' => array(
 		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
 		'method'  => 'POST',
 		'content' => http_build_query($data, '' , '&'),
-		'timeout' => 50000
+		'timeout' => 50
 	)
 );
 $context = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
 echo $result;
+*/
 
+/* cURL 사용 */
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_POST, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
 
 //post방식으로 매개변수 전달하여 php파일 불러오는 방법 사용됨.
+
 ?>
